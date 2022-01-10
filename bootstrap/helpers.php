702cc028bc9a7573a7ccf681d7585eb8b2f8ab6b
G-Sirr <gsirrxz@gmail.com>
2022-01-10 23:07:00 +0300

    Слияние с рабочей версией

---
+++ bootstrap/helpers.php
@@ -0,0 +1,11 @@
+<?php
+if (! function_exists('weekdayDate')) {
+    function weekdayDate ($date) {
+        if ($date == false) {
+            return $date;
+        }
+        $parsed_date = \Carbon\Carbon::parse($date);
+        return $parsed_date->isoFormat('DD.MM.YYYY dd');
+    }
+}
+
