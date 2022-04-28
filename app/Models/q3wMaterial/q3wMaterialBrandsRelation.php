a489977d4918c10dcca59d200d4a01cfe9916b6b
G-Sirr <gsirrxz@gmail.com>
2022-04-28 17:47:42 +0300

    erp-110 [Материальный учет] Доступ ко всем типам материалов в операции "Преобразование"

---
+++ app/Models/q3wMaterial/q3wMaterialBrandsRelation.php
@@ -0,0 +1,14 @@
+<?php
+/**  * @mixin ..\Eloquent  */
+namespace App\Models\q3wMaterial;
+
+use App\Traits\DevExtremeDataSourceLoadable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class q3wMaterialBrandsRelation extends Model
+{
+    use SoftDeletes, DevExtremeDataSourceLoadable;
+
+    protected $guarded = array('id');
+}
