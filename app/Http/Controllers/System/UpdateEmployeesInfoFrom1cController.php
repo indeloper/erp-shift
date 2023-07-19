b76cfa462040fb715d05887570e82d0b8d09845e
G-Sirr <gsirrxz@gmail.com>
2023-07-19 15:27:15 +0300

    BugFix: An error while sending Telegram message

--- app/Http/Controllers/System/UpdateEmployeesInfoFrom1cController.php
+++ app/Http/Controllers/System/UpdateEmployeesInfoFrom1cController.php
@@ -23,23 +23,27 @@ use App\Models\Notification;
 
 class UpdateEmployeesInfoFrom1cController extends Controller
 {
-    function uploadData(Request $request)  
+    function uploadData(Request $request)
     {
-        // Проверка токена временно здесь, т.к. другие способы подразумевают аутентификацию пользователя. 
+        // Проверка токена временно здесь, т.к. другие способы подразумевают аутентификацию пользователя.
         // На первом решили действовать без перенастройки на стороне 1с
-        if(empty($request->input('apiKey')) || $request->input('apiKey') != config('auth.SYNC_1C_API_KEY'))
-        return response()->json([
-            'result' => 'forbidden',
-        ], 403);
 
-        if($request->input('apiAction') == 'employeesSync')
-        $result = $this->employeesSync($request->input('employeeList'));
+        $jsonData = json_decode($request->getContent());
 
-        if($request->input('apiAction') == 'subdivisionsSync')
-        $result = $this->subdivisionsSync($request->input('subdivisionList'));
+        if(empty($jsonData->apiKey) || $jsonData->apiKey != config('auth.SYNC_1C_API_KEY'))
+            return response()->json([
+                'result' => 'forbidden',
+                '$request->getContent()' => $request->getContent()
+            ], 403);
 
-        if($request->input('apiAction') == 'postsSync')
-        $result = $this->postsSync($request->input('postList'));
+        if($jsonData->apiAction == 'subdivisionsSync')
+            $this->subdivisionsSync($jsonData->subdivisionList);
+
+        if($jsonData->apiAction == 'postsSync')
+            $this->postsSync($jsonData->postList);
+
+        if($jsonData->apiAction == 'employeesSync')
+            $this->employeesSync($jsonData->employeeList);
 
         return response()->json([
             'result' => 'ok',
@@ -49,12 +53,12 @@ class UpdateEmployeesInfoFrom1cController extends Controller
 
     function employeesSync($employeeList)
     {
-        $notificationRecipients = User::where('is_su', 1)->get();   
+        $notificationRecipients = User::where('is_su', 1)->get();
 
         foreach ($employeeList as $employee) {
 
             $employee = (object)$employee;
-            
+
             $company = Company::where('company_1c_uid', $employee->organizationUID)->get()->first();
             $employeePost = Employees1cPost::where('post_1c_uid', '=', $employee->employee1CPostUID)->get()->first();
             $employeeSubdivision = Employees1cSubdivision::where('subdivision_1c_uid', '=', $employee->employee1CSubdivisionUID)->get()->first();
@@ -66,63 +70,60 @@ class UpdateEmployeesInfoFrom1cController extends Controller
             }
 
             $formattedBirthday = Carbon::parse($employee->birthday)->format('d.m.Y');
-            $formattedPhone = preg_replace("/[^0-9]/", '', $employee->employeePhone);
+            $formattedPhone = preg_replace("/[^0-9]/", '', trim($employee->employeePhone));
             if (substr($formattedPhone, 0, 1) == 8) {
                 $formattedPhone = substr_replace($formattedPhone, '7', 0, 1);
             }
 
             if (isset($company)) {
-                
+
                 $user = User::withoutGlobalScopes()
                     ->where('inn', '=', trim($employee->employeeINN))
                     ->get()
                     ->first();
 
-                    if (isset($user))
-                    {
-                            DB::statement("update users set " .
-                                "first_name = '" . trim($employee->employeeFirstName) . "', " .
-                                "last_name = '" . trim($employee->employeeLastName) . "', " .
-                                "patronymic = '" . trim($employee->employeePatronymic) . "', " .
-                                "person_phone = '" . $formattedPhone . "', " .
-                                "inn = '" . trim($employee->employeeINN) . "', " .
-                                "gender = '" . trim($employee->employeeGender) . "', " .
-                                "person_phone = '" . $formattedPhone . "', " .
-                                "company = " . $company->id . ", " .
-                                "updated_at = NOW(), " .
-                                "status = " . $userStatus . " " .
-                                "where id = '" . $user->id . "'");
-                            Log::channel('stderr')->info('[info] Обновлен пользователь: ' . $employee->employeeLastName . ' ' . $employee->employeeFirstName . ' ' . trim($employee->employeePatronymic));
-                    } else {
-                        DB::statement('insert into users (first_name,' .
-                                                                'last_name,' .
-                                                                'patronymic,' .
-                                                                'inn,' .
-                                                                'gender,' .
-                                                                'birthday,' .
-                                                                'person_phone,' .
-                                                                'company,' .
-                                                                'created_at,' .
-                                                                'updated_at,' .
-                                                                'status,' .
-                                                                'is_su) ' .
+                if (isset($user))
+                {
+                    DB::statement("update users set " .
+                        "first_name = '" . trim($employee->employeeFirstName) . "', " .
+                        "last_name = '" . trim($employee->employeeLastName) . "', " .
+                        "patronymic = '" . trim($employee->employeePatronymic) . "', " .
+                        "person_phone = '" . $formattedPhone . "', " .
+                        "inn = '" . trim($employee->employeeINN) . "', " .
+                        "gender = '" . trim($employee->employeeGender) . "', " .
+                        "person_phone = '" . $formattedPhone . "', " .
+                        "company = " . $company->id . ", " .
+                        "updated_at = NOW(), " .
+                        "status = " . $userStatus . " " .
+                        "where id = '" . $user->id . "'");
+                } else {
+                    DB::statement('insert into users (first_name,' .
+                        'last_name,' .
+                        'patronymic,' .
+                        'inn,' .
+                        'gender,' .
+                        'birthday,' .
+                        'person_phone,' .
+                        'company,' .
+                        'created_at,' .
+                        'updated_at,' .
+                        'status,' .
+                        'is_su) ' .
                         'values (' .
-                            "'" . trim($employee->employeeFirstName) . "'," .
-                            "'" . trim($employee->employeeLastName) . "'," .
-                            "'" . trim($employee->employeePatronymic) . "'," .
-                            "'" . trim($employee->employeeINN) . "'," .
-                            "'" . trim($employee->employeeGender) . "'," .
-                            "'" . $formattedBirthday . "'," .
-                            "'" . $formattedPhone . "'," .
-                                  $company->id . "," .
-                                  'NOW()' . "," .
-                                  'NOW()' . "," .
-                                  '0' . "," .
-                                  '0' .
+                        "'" . trim($employee->employeeFirstName) . "'," .
+                        "'" . trim($employee->employeeLastName) . "'," .
+                        "'" . trim($employee->employeePatronymic) . "'," .
+                        "'" . trim($employee->employeeINN) . "'," .
+                        "'" . trim($employee->employeeGender) . "'," .
+                        "'" . $formattedBirthday . "'," .
+                        "'" . $formattedPhone . "'," .
+                        $company->id . "," .
+                        'NOW()' . "," .
+                        'NOW()' . "," .
+                        '0' . "," .
+                        '0' .
                         ')');
-
-                        Log::channel('stderr')->info('[info] Добавлен новый пользователь: ' . $employee->employeeLastName . ' ' . $employee->employeeFirstName . ' ' . trim($employee->employeePatronymic));
-                    }
+                }
 
                 $user = User::withoutGlobalScopes()
                     ->where('inn', '=', trim($employee->employeeINN))
@@ -130,8 +131,8 @@ class UpdateEmployeesInfoFrom1cController extends Controller
                     ->first();
 
                 $lastEmployeesId = Employee::orderByDesc('id')->first()->id;
-                
-                $updated_employee = Employee::updateOrCreate(
+
+                $updatedEmployee = Employee::updateOrCreate(
                     [
                         'employee_1c_uid' => $employee->employeeUID,
                     ],
@@ -147,32 +148,26 @@ class UpdateEmployeesInfoFrom1cController extends Controller
                         'report_group_id' => null
                     ]
                 );
-                
-                if($updated_employee->id > $lastEmployeesId){
+
+                if ($updatedEmployee->id > $lastEmployeesId){
                     foreach($notificationRecipients as $recipient){
                         Notification::create([
-                            'name' => 'Добавлен сотрудник: ' . $employee->employeeName . ' ' . $updated_employee->employee1cPost->name,
+                            'name' => 'Добавлен сотрудник: ' . $employee->employeeName . ' – ' . $employeePost->name,
                             'user_id' => $recipient->id,
                             'type' => 0,
                         ]);
                     }
                 }
-
             }
         }
-        
+
     }
 
 
     function subdivisionsSync($subdivisionList)
     {
         foreach ($subdivisionList as $subdivision) {
-
-            $subdivision = (object)$subdivision;
-
             $company = Company::where('company_1c_uid', $subdivision->organizationUID)->get()->first();
-
-            Log::channel('stderr')->info($subdivision->subdivisionParentUID);
 
             $subdivisionParentId = null;
 
@@ -223,25 +218,20 @@ class UpdateEmployeesInfoFrom1cController extends Controller
                 );
 
                 $post->inflection = (object)$post->inflection;
-
-                    Log::channel('stderr')->info('[info] ' . $post->inflection->nominative);
-                    Employees1cPostInflection::updateOrCreate(
-                        [
-                            'post_id' => $employeePost->id
-                        ],
-                        [
-                            'nominative' => $post->inflection->nominative,
-                            'genitive' => $post->inflection->genitive,
-                            'dative' => $post->inflection->dative,
-                            'accusative' => $post->inflection->accusative,
-                            'ablative' => $post->inflection->ablative,
-                            'prepositional' => $post->inflection->prepositional
-                        ]
-                    );
-
+                Employees1cPostInflection::updateOrCreate(
+                    [
+                        'post_id' => $employeePost->id
+                    ],
+                    [
+                        'nominative' => $post->inflection->nominative,
+                        'genitive' => $post->inflection->genitive,
+                        'dative' => $post->inflection->dative,
+                        'accusative' => $post->inflection->accusative,
+                        'ablative' => $post->inflection->ablative,
+                        'prepositional' => $post->inflection->prepositional
+                    ]
+                );
             }
         }
     }
-
-
 }
