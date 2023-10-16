76dc8568c7aca5734345adcabd8952527da17239
a.ivanov <a.ivanov@sk-gorod.com>
2023-10-16 15:32:36 +0300

    erp-182. Ограничить доступные модели транспорта при выборе бренда

--- resources/views/tech_accounting/technic/ourTechnicList/desktop/components/dataGridEditForm01Group1Elems.blade.php
+++ resources/views/tech_accounting/technic/ourTechnicList/desktop/components/dataGridEditForm01Group1Elems.blade.php
@@ -31,8 +31,15 @@
             dataField: "technic_brand_id",
             colSpan: 1,
             editorOptions: {
-                onSelectionChanged(){
-                    console.log(technicModelsStore);
+                onSelectionChanged(e){
+                    if(e.selectedItem.id) {
+                        let interval = setInterval(() => {
+                            if($('#technic_brand_model_id').dxSelectBox('instance')) {
+                                clearInterval(interval)
+                                $('#technic_brand_model_id').dxSelectBox('instance')?.option('dataSource', $('#technic_brand_model_id').dxSelectBox('instance')?.option('dataSource').store.__rawData.filter(el=>el.technic_brand_id ===e.selectedItem.id));
+                            }
+                        }, 100)
+                    }
                 }
             },
             validationRules: [{
@@ -44,6 +51,12 @@
         {
             dataField: "technic_brand_model_id",
             colSpan: 1,
+            editorType: 'dxSelectBox',
+            editorOptions: {
+              elementAttr: {
+                id: 'technic_brand_model_id'
+              }  
+            },
             validationRules: [{
                 type: 'required',
                 message: 'Укажите значение',
