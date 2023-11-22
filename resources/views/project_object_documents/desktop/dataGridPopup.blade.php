53e595c66d6e1ceda1a75c9d3332dcbd9321fc7f
Иванов Антон <a.ivanov@sk-gorod.com>
2023-11-22 17:36:50 +0300

    erp-220 [ERP] done

--- resources/views/project_object_documents/desktop/dataGridPopup.blade.php
+++ resources/views/project_object_documents/desktop/dataGridPopup.blade.php
@@ -119,7 +119,7 @@
             valueExpr: "id",
             displayExpr: "name",
             value: editingRowStatusId,
-
+            searchEnabled: true,
             itemTemplate(data) {
                 return $(`
                             <div style="display:flex; align-items:center">
@@ -232,6 +232,7 @@
                         displayExpr: 'user_full_name',
                         label: data.label,
                         labelMode: "floating",
+                        searchEnabled: true,
                         onValueChanged(e) {
                             editingRowTypeStatusOptions_tmp.push(
                                 {
