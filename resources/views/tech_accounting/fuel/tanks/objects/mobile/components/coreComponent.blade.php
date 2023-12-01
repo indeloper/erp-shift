c7c44261a57d8a8098789295de601e92ec96a8c0
a.ivanov <a.ivanov@sk-gorod.com>
2023-12-01 14:59:22 +0300

    erp-243 [ERP] добавить иконки на кнопки моб бочки done

--- resources/views/tech_accounting/fuel/tanks/objects/mobile/components/coreComponent.blade.php
+++ resources/views/tech_accounting/fuel/tanks/objects/mobile/components/coreComponent.blade.php
@@ -3,12 +3,12 @@
         $('#entitiesListMobile').dxList({
             dataSource: entitiesDataSource,
             searchEnabled: true,
-            
+
             itemTemplate(data) {
 
                 const listElement = $('<div>')
 
-                const listElementRowWrapper = 
+                const listElementRowWrapper =
                     $('<div>').addClass('list-element-wrapper').appendTo(listElement);
 
                     $('<div>')
@@ -19,12 +19,12 @@
                 const textInfoWrapper = $('<div>')
                         .addClass('list-element-item-text-info-wrapper')
                         .appendTo(listElementRowWrapper)
-                    
+
                 let colorClass = ''
                 if(data.fuel_level > 0)
-                    colorClass = 'text-color-green' 
+                    colorClass = 'text-color-green'
                 if(data.fuel_level < 0)
-                    colorClass = 'text-color-red' 
+                    colorClass = 'text-color-red'
 
                 if(data.awaiting_confirmation) {
                     $('<p>')
@@ -32,14 +32,14 @@
                         .css('font-weight', 'bold')
                         .text('Требуется подтверждение')
                         .appendTo(textInfoWrapper)
-                    
+
                     $('<p>')
                         .addClass('list-element-item-info-text text-color-red')
                         .css('margin-bottom', '10px')
                         .text(`Ответственный: ${data.responsible.full_name}`)
                         .appendTo(textInfoWrapper)
                 }
-               
+
                 $('<p>')
                     .addClass('list-element-item-info-text ')
                     .html(`<b>Остаток топлива: </b><span class="${colorClass}">${new Intl.NumberFormat('ru-RU').format(data.fuel_level)} л</span>`)
@@ -50,51 +50,58 @@
                     .html(`<b>Объект: </b>${data.object?.short_name}`)
                     .appendTo(textInfoWrapper)
 
-                const listElementRowWrapper2 = 
+                const listElementRowWrapper2 =
                     $('<div>').addClass('list-element-wrapper').appendTo(listElement);
 
                 const buttonsWrapper = $('<div>').addClass('buttons-wrapper').appendTo(listElementRowWrapper2)
 
                 $('<div>').dxButton({
-                        text: 'Приход',
-                        visible: userPermissions.create_fuel_tank_flows_for_reportable_tanks || userPermissions.create_fuel_tank_flows_for_any_tank,
-                        onClick() {
-                            editingRowId = data.id;
-                            shownMobileFormType = 'increaseFuelForm';
-                            showIncreaseFuelPopup();
-                        }
+                    template(data, container) {
+                        container.append('<div class="tab-template-header-wrapper"><div class="fa fa-arrow-up tab-template-header-icon-elem text-color-green"></div><div>Приход</div></div>')
+                    },
+                    visible: userPermissions.create_fuel_tank_flows_for_reportable_tanks || userPermissions.create_fuel_tank_flows_for_any_tank,
+                    onClick() {
+                        editingRowId = data.id;
+                        shownMobileFormType = 'increaseFuelForm';
+                        showIncreaseFuelPopup();
+                    }
                 }).appendTo(buttonsWrapper)
 
                 $('<div>').dxButton({
-                        text: 'Перемещение',
-                        visible: Boolean( Boolean("{{App::environment('local')}}") ? true : +data.responsible_id === +authUserId),
-                        onClick() {
-                            editingRowId = data.id;
+                    template(data, container) {
+                        container.append('<div class="tab-template-header-wrapper"><div class="fas fa-exchange-alt tab-template-header-icon-elem text-color-blue"></div><div>Перемещение</div></div>')
+                    },
+                    visible: Boolean(Boolean("{{App::environment('local')}}") ? true : +data.responsible_id === +authUserId),
+                    onClick() {
+                        editingRowId = data.id;
 
-                            if (data.awaiting_confirmation) {
-                                shownMobileFormType = 'movingConfirmationTankForm';
-                                showMovingConfirmationTankPopup(data);
-                            }
-                            else {
-                                shownMobileFormType = 'movingTankForm';
-                                showMovingTankPopup();
-                            }
-                        } 
+                        if (data.awaiting_confirmation) {
+                            shownMobileFormType = 'movingConfirmationTankForm';
+                            showMovingConfirmationTankPopup(data);
+                        } else {
+                            shownMobileFormType = 'movingTankForm';
+                            showMovingTankPopup();
+                        }
+                    }
                 }).appendTo(buttonsWrapper)
 
                 $('<div>').dxButton({
-                        text: 'Расход',
-                        visible: userPermissions.create_fuel_tank_flows_for_reportable_tanks || userPermissions.create_fuel_tank_flows_for_any_tank,
-                        onClick() {
-                            editingRowId = data.id;
-                            shownMobileFormType = 'decreaseFuelForm';
-                            showDecreaseFuelPopup();
-                        }
+                    template(data, container) {
+                        container.append('<div class="tab-template-header-wrapper"><div class="fa fa-arrow-down tab-template-header-icon-elem text-color-red"></div><div>Расход</div></div>')
+                    },
+                    visible: userPermissions.create_fuel_tank_flows_for_reportable_tanks || userPermissions.create_fuel_tank_flows_for_any_tank,
+                    onClick() {
+                        editingRowId = data.id;
+                        shownMobileFormType = 'decreaseFuelForm';
+                        showDecreaseFuelPopup();
+                    }
                 }).appendTo(buttonsWrapper)
+
 
                 return listElement;
             },
 
         });
+
     })
 </script>
