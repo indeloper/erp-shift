1e5e000667d1fa084599c9038aabbc53aa766ecf
G-Sirr <gsirrxz@gmail.com>
2023-12-08 14:48:18 +0300

    erp-256 [ERP] Коммерческий блок. Стилизация и примечания.

--- app/Models/CommercialOffer/CommercialOfferManualRequirement.php
+++ app/Models/CommercialOffer/CommercialOfferManualRequirement.php
@@ -3,8 +3,11 @@
 namespace App\Models\CommercialOffer;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CommercialOfferManualRequirement extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = array('id');
 }
