e63238d21702a51af13974bca8d7033fcde53999
Антон Иванов <a.ivanov@sk-gorod.com>
2023-12-15 11:49:05 +0000

    erp-268. [ERP] - добавить тип контрагента - рендодатель техники - и сократить...

---
+++ database/migrations/2023_12_15_141422_add_technic_lessor_contractor_type.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddTechnicLessorContractorType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('contractor_types')->insert($this->getNewEntrises());
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    { 
+        foreach(self::NEW_ENTRIES as $newEntry) {
+            DB::table('contractor_types')->where('codename', $newEntry['codename'])->delete();
+        }
+    }
+
+    public function getNewEntrises() {
+        $newEntries = self::NEW_ENTRIES;
+        foreach($newEntries as $key=>$newEntry) {
+            $newEntries[$key]['created_at'] = now();
+            $newEntries[$key]['updated_at'] = now();
+        }
+        return $newEntries;
+    }
+
+    const NEW_ENTRIES = [
+        [
+            'name' => 'Арендодатель техники', 
+            'slug' => 'technic_lessor',
+        ],
+    ];
+}
