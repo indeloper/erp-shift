eed20da9c177f97d388f063651272da08dc2a418
Антон Иванов <a.ivanov@sk-gorod.com>
2023-12-19 19:04:44 +0000

    erp-264 Подтверждение топливной емкости

--- app/Telegram/TelegramApi.php
+++ app/Telegram/TelegramApi.php
@@ -8,17 +8,19 @@ class TelegramApi {
     
     protected $method;
     protected $data;
+    protected $options;
 
-    public function __construct($method, $data)
+    public function __construct($method, $data, $options = [])
     {
         $this->method = $method;
         $this->data = $data;
+        $this->options = $options;
         $this->handle();
     }
 
     public function handle()
     {
         $url = 'https://api.telegram.org/bot'.config('telegram.bot_token').'/'. $this->method;
-        dispatch(new TelegramApiRequest($url, $this->data));
+        dispatch(new TelegramApiRequest($url, $this->data, $this->options));
     }
 }
