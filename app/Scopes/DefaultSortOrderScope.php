de9a5a587350d82165bd511f6c347a2213046d97
G-Sirr <gsirrxz@gmail.com>
2024-01-12 09:49:06 +0300

    erp-287  [ERP] Трейт для сортировки моделей по полям Выполнено. Дополнительно внесена команда для генерации моделей для erp (erp:make:model)

---
+++ app/Scopes/DefaultSortOrderScope.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class DefaultSortOrderScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+        if (!$builder->getQuery()->orders) {
+            if (isset($model->defaultSortOrder)){
+                foreach ($model->defaultSortOrder as $key => $value){
+                    $builder->orderBy($model->getTable().'.'.$key, $value);
+                }
+            }
+        }
+    }
+}
