9ba01062a53ddbd6d704ee3e31cb1f993084e0a4
Тихонов Сергей <gsirrxz@gmail.com>
2024-06-13 08:38:21 +0000

    Update file AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,6 +29,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        \URL::forceScheme('https');
         if (config('app.env') != 'production') {
             DB::listen(function ($query) {
                 File::append(
