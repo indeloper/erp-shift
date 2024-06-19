64cbe1c97f559c930025dd4a8a36ecdde88032ba
G-Sirr <gsirrxz@gmail.com>
2024-06-19 12:58:25 +0300

    erp-362 [ERP] Устранение багов при переходе на Laravel 11 и PHP 8.2

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,8 @@ runuser -u www-data -- php artisan config:clear
 echo "Caching config"
 runuser -u www-data -- php artisan config:cache
 
+runuser -u www-data -- php artisan migrate --force
+
 chgrp -R www-data .
 chmod 764 "./storage/logs/laravel.log"
 
